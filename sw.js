79f718320c5722426f58103c3e08bb4765302b6b
supermancash <alexanderio.emilio@gmail.com>
2021-06-06 14:58:58 +0200

    adding onclick to notification

--- sw.js
+++ sw.js
@@ -1,6 +1,30 @@
-self.addEventListener('push', () =>{
+self.addEventListener('push', (e) =>{
     let options = {
+        body:'',
+        icon: '',
+        vibrate:[100, 50, 100],
 
     }
-    self.registration.showNotification('test message', options);
+    e.waitUntil(self.registration.showNotification('test message', options));
+})
+
+self.addEventListener('notificationclick', (e) =>{
+    let url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=OfficialRickAstleyOfficialRickAstleyOffiziellerK%C3%BCnstlerkanal';
+    e.notification.close(); // Android needs explicit close.
+    e.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 })
