248ca0ecb3fcedb7b0f26c6a4efed7a73f80bd99
supermancash <alexanderio.emilio@gmail.com>
2021-06-16 11:26:04 +0200

    updating api url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function subscribe() {
     let endpointDiv = document.getElementById("endpoint");
 
 
-    await fetch("https://73602027c10e.ngrok.io/api/subscribers", {
+    await fetch("https://9e59b2c6fce0.ngrok.io/api/subscribers", {
         method: 'POST',
         credentials: 'omit',
         mode: 'cors',
